803b18b16f55ebdb8175ca687c1b5eebe8c19dfe
Aline Trt <alinetorteau@gmail.com>
2024-11-26 22:08:57 +0100

    page annonce

--- annonces.php
+++ annonces.php
@@ -15,8 +15,22 @@ $listings = getListings();
     <div class="col-md-3">
         <form action="" method="get">
             <h2>Filtres</h2>
-            <div>
+            <div class="p-3 border-bottom">
                 <input type="text" name="search" id="search" class="form-control" placeholder="rechercher">
+            </div>
+            <div class="p-3 border-bottom">
+                <label for="price">Prix</label>
+                <div class="input-group">
+                    <input type="number" name="min_price" id="min_price" class="form-control" placeholder="prix minimum">
+                    <span class="input-group-text">€</span>
+                </div>
+                <div class="input-group">
+                    <input type="number" name="max" id="max" class="form-control" placeholder="prix maximum">
+                    <span class="input-group-text">€</span>
+                </div>
+            </div>
+            <div>
+                <button type="submit" class="btn btn-primary w-100">Rechercher</button>
             </div>
         </form>
     </div>
--- libs/listing.php
+++ libs/listing.php
@@ -3,8 +3,14 @@
 function getListings(): array
 {
     return [
-        ["title" => "test1", "price" => 30, "image" => "crystal.jpg"],
-        ["title" => "test2", "price" => 50, "image" => "crystal.jpg"],
-        ["title" => "test3", "price" => 10, "image" => "crystal.jpg"],
+        ["title" => "test1", "price" => 30, "image" => "/crystal.jpg", "description" => "Test description"],
+        ["title" => "test2", "price" => 50, "image" => "crystal.jpg", "description" => "Test description"],
+        ["title" => "test3", "price" => 10, "image" => "crystal.jpg", "description" => "Test description"],
     ];
+}
+
+function getListingById(int $id): array
+{
+    $listings = getListings();
+    return $listings[$id];
 }
